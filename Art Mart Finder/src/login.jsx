74c79f08e20f9b97e29ebe8ef5473940235cd985
SabrinaKuang <iamsabrinakuang@berkeley.edu>
2025-07-30 16:11:37 -0700

    Set up Routing + pages

---
+++ Art Mart Finder/src/login.jsx
@@ -0,0 +1,6 @@
+function Login() {
+    return <h2>this is the login page!</h2>;
+  }
+  
+  export default Login;
+  
---
+++ Art Mart Finder/src/search.jsx
@@ -0,0 +1,6 @@
+function Search() {
+    return <h2>This is search page!</h2>;
+  }
+  
+  export default Search;
+  
