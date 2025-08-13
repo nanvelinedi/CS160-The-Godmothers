9257c13b9eed48ab3b8663c4be297d928705d0db
Jessie Liu <jess04liu@berkeley.edu>
2025-08-13 04:32:37 -0700

    pls work

--- website/src/profiles/lunapfp.jsx
+++ website/src/profiles/lunapfp.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LunaPfp() {
     return (
         <div>
-            {/* Add your profile picture or content here hello pls work */}
+            {/* Add your profile picture or content hello */}
             <img src="path-to-luna-profile-picture.jpg" alt="Luna Profile" />
         </div>
     );
